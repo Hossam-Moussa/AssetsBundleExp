c3d33ec9a53296b57a53df87c24b6659d9731282
Hossam Moussa <hossammoussaallam@gmail.com>
2018-09-25 23:57:47 +0300

    just another hint for the git tutorial nvm it

---
+++ Assets Bundle Exp/Assets/AssetDownloader.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AssetDownloader : MonoBehaviour 
+{
+	
+	GameObject Asset;
+	public string url;
+	public Transform firstpos;
+	public Transform secpos;
+	public Transform thpos;
+	private float loadin;
+	IEnumerator LoadBundle(string assetname, Transform tra) 
+	{
+		while (!Caching.ready)
+		{
+			yield return null;
+		}
+
+		//Begin download
+		WWW www = WWW.LoadFromCacheOrDownload (url, 0);
+		yield return www;
+
+		//Load the downloaded bundle
+		AssetBundle bundle = www.assetBundle;
+
+		//Load an asset from the loaded bundle
+		AssetBundleRequest bundleRequest = bundle.LoadAssetAsync (assetname, typeof(GameObject));
+		yield return bundleRequest;
+
+		//get object
+		GameObject obj = bundleRequest.asset as GameObject;
+
+		Asset = Instantiate(obj, tra.position, Quaternion.identity) as GameObject;
+
+		bundle.Unload(false);
+		www.Dispose();
+		loadin++;
+	}
+
+	void Start()
+	{
+		loadin = 0f;
+
+		StartCoroutine(LoadBundle("Sphere", firstpos));    
+	}
+	void Update(){
+		if (loadin == 1f) {
+
+			StartCoroutine (LoadBundle ("Cube", secpos));  
+		} else if (loadin == 2f) {
+
+			StartCoroutine(LoadBundle("Capsule", thpos));  
+		}
+	}
+}
